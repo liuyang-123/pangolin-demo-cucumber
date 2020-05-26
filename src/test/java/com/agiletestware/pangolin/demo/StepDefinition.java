b705e7bf9335a88e9c139634bdf2fdad886390e3
LiuYang <liuyang123@gmail.com>
2020-05-26 20:05:42 +0800

    Use bing for test

--- src/test/java/com/agiletestware/pangolin/demo/StepDefinition.java
+++ src/test/java/com/agiletestware/pangolin/demo/StepDefinition.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class StepDefinition {
 
 	public static WebDriver webDriver = null;
-	public static final String GOOGLE_URL = "http://www.google.com";
+	public static final String GOOGLE_URL = "http://www.bing.com";
 
 	@Given("^I have gone to the Google page$")
 	public void goToTheGooglePage() {
@@ -44,12 +44,12 @@ public class StepDefinition {
 
 	@And("^click the Search Button$")
 	public void clickSearch() {
-		webDriver.findElement(By.name("btnK")).submit();
+		webDriver.findElement(By.name("go")).submit();
 	}
 
 	@Then("^agiletestware should be mentioned in the results$")
 	public void displayResults() {
-		final String agileTitle = webDriver.findElement(By.className("LC20lb")).getText();
+		final String agileTitle = webDriver.findElement(By.xpath("//a[@href='https://www.agiletestware.com/']")).getText();
 		assertEquals("Agiletestware - Software for QA and Development Tools", agileTitle);
 	}
 
