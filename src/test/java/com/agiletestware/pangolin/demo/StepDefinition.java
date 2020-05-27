7b64eb1dea0a7cfa2bd68532d5951586bf07fb70
LiuYang <liuyang123@gmail.com>
2020-05-27 16:40:51 +0800

    Update

--- src/test/java/com/agiletestware/pangolin/demo/StepDefinition.java
+++ src/test/java/com/agiletestware/pangolin/demo/StepDefinition.java
@@ -71,14 +71,13 @@ public class StepDefinition {
 
 	@After
 	public void afterScenario(final Scenario scenario) {
+		if (scenario.isFailed()) {
+			scenario.embed(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES), "image/png");
+		}
+
 		if (webDriver != null) {
 			webDriver.close();
 			webDriver.quit();
 		}
-
-        if (scenario.isFailed()) {
-            scenario.embed(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES), "image/png");
-            fail("The step failed, So the 'after' step should be failure also so that we can upload screenshot.");
-        }
 	}
 }
